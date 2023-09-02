b598d4ef05feace708c285c932c5028ca6516aec
Shivam Kumar Jha <shivamjha779@gmail.com>
2023-09-03 04:34:20 +0530

    making Models and testing

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
+#include "Model/RubiksCube3dArray.cpp"
+#include "Model/RubiksCube1dArray.cpp"
+#include "Model/RubiksCubeBitboard.cpp"
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    RubiksCube3dArray object3DArray;
+    RubiksCube1dArray object1dArray;
+    RubiksCubeBitboard objectBitboard;
+
+    objectBitboard.print();
+
     return 0;
 }
