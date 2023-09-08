c321efe648f9a074defea3d9b65450c9f7961caf
Shivam Kumar Jha <shivamjha779@gmail.com>
2023-09-08 19:16:26 +0530

    solver testing

--- main.cpp
+++ main.cpp
@@ -1,15 +1,34 @@
 #include <bits/stdc++.h>
 #include "Model/RubiksCube3dArray.cpp"
-#include "Model/RubiksCube1dArray.cpp"
-#include "Model/RubiksCubeBitboard.cpp"
+//#include "Model/RubiksCube1dArray.cpp"
+//#include "Model/RubiksCubeBitboard.cpp"
+#include "Solver/DFSSolver.h"
+#include "Solver/BFSSolver.h"
 int main() {
-    RubiksCube3dArray object3DArray;
-    RubiksCube1dArray object1dArray;
-    RubiksCubeBitboard objectBitboard;
+//    RubiksCube3dArray object3DArray;
+//    RubiksCube1dArray object1dArray;
+//    RubiksCubeBitboard objectBitboard;
 
-    objectBitboard.print();
-    if (object3DArray.isSolved()) cout << "SOLVED\n\n";
-    else cout << "NOT SOLVED\n\n";
+//    objectBitboard.print();
+//    if (object3DArray.isSolved()) cout << "SOLVED\n\n";
+//    else cout << "NOT SOLVED\n\n";
+
+    // DFS Solver Testing __________________________________________________________________________________________
+    RubiksCube3dArray cube;
+    cube.print();
+
+    vector<RubiksCube::MOVE> shuffle_moves = cube.randomShuffleCube(6);
+    for (auto move: shuffle_moves) cout << cube.getMove(move) << " ";
+    cout << "\n";
+    cube.print();
+
+    DFSSolver<RubiksCube3dArray, Hash3d> dfsSolver(cube, 8);
+    vector<RubiksCube::MOVE> solve_moves = dfsSolver.solve();
+
+    for (auto move: solve_moves) cout << cube.getMove(move) << " ";
+    cout << "\n";
+    dfsSolver.rubiksCube.print();
+
 
     return 0;
 }
